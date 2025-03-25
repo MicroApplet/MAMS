94594c7ca28d340ed5d3497d2189d7d32d946194
Asial Jim <asialjim@hotmail.com>
2025-03-25 14:49:24 +0800

    init

--- user/user-auth-web/package.sh
+++ user/user-auth-web/package.sh
@@ -2,3 +2,4 @@ cp /app/docker/mams/user/user-auth-web/target/*.jar /app/docker/mams/user/user-a
 cp /app/docker/mams/user/user-auth-web/Dockerfile /app/docker/mams/user/user-auth-web
 cp /app/docker/mams/user/user-auth-web/docker-run.sh /app/docker/mams/user/user-auth-web
 cp /app/docker/mams/mams.env /app/docker/mams/user/user-auth-web
+cp /home/mams/.env/mams.env /app/docker/mams/.env/mams.env
