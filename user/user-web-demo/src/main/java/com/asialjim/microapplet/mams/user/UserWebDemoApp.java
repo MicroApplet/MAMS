cd79fcd99117ed320b75d8364ca063a2d381ae24
Asial Jim <asialjim@hotmail.com>
2025-04-15 22:24:32 +0800

    init
    Signed-off-by: Asial Jim <asialjim@hotmail.com>

--- user/user-web-demo/src/main/java/com/asialjim/microapplet/mams/user/UserWebDemoApp.java
+++ user/user-web-demo/src/main/java/com/asialjim/microapplet/mams/user/UserWebDemoApp.java
@@ -1,23 +1,25 @@
 /*
- * Copyright 2014-2025 <a href="mailto:asialjim@qq.com">Asial Jim</a>
+ *  Copyright 2014-2025 <a href="mailto:asialjim@qq.com">Asial Jim</a>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package com.asialjim.microapplet.mams.user;
 
 import com.asialjim.microapplet.common.application.App;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 启动器
@@ -26,6 +28,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  * @since 2025/4/11, &nbsp;&nbsp; <em>version:1.0</em>
  */
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserWebDemoApp {
     public static void main(String[] args) {
