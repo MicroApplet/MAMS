b74d8b8a21e3d80fae2fef1175b741f9e071b5d4
Asial Jim <asialjim@hotmail.com>
2025-04-28 17:55:06 +0800

    init

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+echo "当前目录"
+pwd
+ls -la
 # applet/applet-web
 # shellcheck disable=SC2164
 cd ~
+echo "home目录"
+pwd
+ls -la
 mkdir -p /app/build/applet/web
 # shellcheck disable=SC2164
 cd ./applet/applet-web
@@ -11,6 +17,9 @@ sh start.sh
 # channel/channel-wechat/channel-wechat-official/channel-wechat-official-web
 # shellcheck disable=SC2164
 cd ~
+echo "home目录"
+pwd
+ls -la
 mkdir -p /app/build/channel/wechat/official/web
 # shellcheck disable=SC2164
 cd ./channel/channel-wechat/channel-wechat-official/channel-wechat-official-web
@@ -20,6 +29,9 @@ sh start.sh
 # user/user-web
 # shellcheck disable=SC2164
 cd ~
+echo "home目录"
+pwd
+ls -la
 mkdir -p /app/build/user/web
 # shellcheck disable=SC2164
 cd ./user/user-web
