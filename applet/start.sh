8dd01433b56e118b4a3546a6aa7baaf40d83cb5a
Asial Jim <asialjim@hotmail.com>
2025-04-28 13:56:46 +0800

    init

--- applet/start.sh
+++ applet/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-cp ./applet-web/target/AppletApplication.jar /app/build/applet/AppletApplication.jar
+pwd
+ls -la
+cp ./applet-web/target/AppletApplication.jar /app/build/applet
 cp ./*.sh /app/build/applet
 cp ./Dockerfile /app/build/applet/Dockerfile
