2444af022bab12c06e6dcd592a478dc22e1e0212
Asial Jim <asialjim@hotmail.com>
2025-05-01 15:26:47 +0800

    init
    Signed-off-by: Asial Jim <asialjim@hotmail.com>

--- user/user-chl/user-chl-wechat/user-chl-wechat-common/src/main/java/com/asialjim/microapplet/mams/user/chl/wechat/strategy/WeChatUserAuthenticationStrategy.java
+++ user/user-chl/user-chl-wechat/user-chl-wechat-common/src/main/java/com/asialjim/microapplet/mams/user/chl/wechat/strategy/WeChatUserAuthenticationStrategy.java
@@ -1,23 +1,25 @@
 /*
- * Copyright 2014-2025 <a href="mailto:asialjim@qq.com">Asial Jim</a>
+ *  Copyright 2014-2025 <a href="mailto:asialjim@qq.com">Asial Jim</a>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package com.asialjim.microapplet.mams.user.chl.wechat.strategy;
 
 import com.asialjim.microapplet.common.utils.PasswordStorage;
-import com.asialjim.microapplet.mams.channel.base.*;
+import com.asialjim.microapplet.mams.channel.base.ChlAppType;
+import com.asialjim.microapplet.mams.channel.base.ChlType;
+import com.asialjim.microapplet.mams.channel.base.ChlTypeResCode;
 import com.asialjim.microapplet.mams.user.command.UserLoginCommand;
 import com.asialjim.microapplet.mams.user.command.UserRegCommand;
 import com.asialjim.microapplet.mams.user.domain.agg.SessionUser;
@@ -25,8 +27,8 @@ import com.asialjim.microapplet.mams.user.domain.agg.UserAggRoot;
 import com.asialjim.microapplet.mams.user.domain.strategy.BaseUserAuthenticateStrategy;
 import com.asialjim.microapplet.mams.user.infrastructure.repository.UserChlRepository;
 import com.asialjim.microapplet.mams.user.infrastructure.repository.UserMainRepository;
-import com.asialjim.microapplet.mams.user.vo.UserRegOrLoginReq;
 import com.asialjim.microapplet.mams.user.pojo.UserChl;
+import com.asialjim.microapplet.mams.user.vo.UserRegOrLoginReq;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
@@ -57,7 +59,7 @@ public class WeChatUserAuthenticationStrategy extends BaseUserAuthenticateStrate
             return;
 
         for (WeChatUserLoginStrategy item : weChatUserAuthenticationStrategies) {
-            WeChatChlAppType chlAppType = item.chlAppType();
+            ChlAppType chlAppType = item.chlAppType();
             if (Objects.nonNull(this.strategyMap.get(chlAppType)))
                 throw new IllegalStateException("微信渠道：" + chlAppType + "存在多个用户注册策略，请合并该渠道策略");
             this.strategyMap.put(item.chlAppType(), item);
@@ -67,7 +69,7 @@ public class WeChatUserAuthenticationStrategy extends BaseUserAuthenticateStrate
     @NonNull
     @Override
     public ChlType supportType() {
-        return NormalChlType.WeChat;
+        return ChlType.WeChat;
     }
 
     @Override
@@ -106,6 +108,7 @@ public class WeChatUserAuthenticationStrategy extends BaseUserAuthenticateStrate
 
         UserRegOrLoginReq req = command.getReq();
         SessionUser login = strategy.login(command);
+        // TODO
 
         return null;
     }
