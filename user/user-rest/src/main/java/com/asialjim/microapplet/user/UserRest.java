b2869429ca2af5599a345e027f45845ebde91b1b
AsialJim <asialjim@hotmail.com>
2026-07-01 06:11:46 +0000

    update

--- user/user-rest/src/main/java/com/asialjim/microapplet/user/UserRest.java
+++ user/user-rest/src/main/java/com/asialjim/microapplet/user/UserRest.java
@@ -28,7 +28,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class UserRest {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         App.voidStart(UserRest.class, args);
     }
 }
